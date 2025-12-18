f85fc6e21e314465054ac05dc6ef6d6dfff62302
santiago-h2o <maxcuello123@gmail.com>
2025-12-18 17:19:58 -0500

    Se crea el menu en version de mobile

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './index.css'
 import { LoadingScreen } from './components/loading/LoadingScreen'
 import { useState } from 'react';
 import { Navbar } from './components/navbar/Navbar';
+import MobileMenu from './components/mobileMenu/MobileMenu';
 
 const App = () => {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -14,6 +15,8 @@ const App = () => {
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}{" "}
         <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? 'opacity-100' : 'opacity-0'} bg-black text-gray-100`}>
           <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+          <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+          
         </div>
     </>
   )
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,10 +6,8 @@ export const Navbar = ({menuOpen, setMenuOpen}) => {
         document.body.style.overflow = menuOpen ? 'hidden' : '';
     }, [menuOpen]);
   
-
-
     return (
-    <div className='fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg'>
+    <nav className='fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg'>
         <div className='max-w-5xl mx-auto px-4'>
             <div className='flex justify-between items-center h-16'>
                 <a href="#home" className='font-mono text-xl font-bold text-white'>Santiago <span className='text-blue-500'>Cuello</span> Aguas </a>
@@ -28,7 +26,7 @@ export const Navbar = ({menuOpen, setMenuOpen}) => {
 
             </div>
         </div>
-    </div>
+    </nav>
   )
 }
 
