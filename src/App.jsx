b1fcd284517d2fcc9c17c9eb90e6273ab9ff8e46
santiago-h2o <maxcuello123@gmail.com>
2025-12-18 16:54:24 -0500

    Se crea el header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import { LoadingScreen } from './components/loading/LoadingScreen'
 import './App.css'
 import './index.css'
+import { LoadingScreen } from './components/loading/LoadingScreen'
 import { useState } from 'react';
+import { Navbar } from './components/navbar/Navbar';
 
 const App = () => {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
 
   return (
     <>
-      {
-        !isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>
-      }
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}{" "}
+        <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? 'opacity-100' : 'opacity-0'} bg-black text-gray-100`}>
+          <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        </div>
     </>
   )
 }
