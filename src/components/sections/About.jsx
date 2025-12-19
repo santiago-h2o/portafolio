0c486a42a42f7cb267223cf355e22ec60975caf1
santiago-h2o <maxcuello123@gmail.com>
2025-12-18 22:38:09 -0500

    Se crea animacion de entrada teniendo en cuenta el scroll

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,3 +1,4 @@
+import { RevealOnScroll } from "../scroll/RevealOnScroll";
 
 export const About = () => {
 
@@ -6,6 +7,8 @@ export const About = () => {
 
     return (
         <section id="about" className="min-h-screen flex items-center justify-center py-20">
+              <RevealOnScroll>
+      
             <div className="max-w-3xl mx-auto px-4">
                 <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">Sobre mi</h2>
 
@@ -86,6 +89,8 @@ export const About = () => {
                 </div>
 
             </div>
+                    </RevealOnScroll>
+
         </section>
     )
 }
