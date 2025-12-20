f5c63beda51ad2839b66a80e6ad5f5dc5dbff2ec
santiago-h2o <maxcuello123@gmail.com>
2025-12-19 20:12:46 -0500

    Se comenta el fondo de threejs

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import AnimatedBackground from './components/AnimatedBackground'
+// import AnimatedBackground from './components/AnimatedBackground'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <>
-      <AnimatedBackground />
+      {/* <AnimatedBackground /> */}
       <App />
     </>
   </StrictMode>,
