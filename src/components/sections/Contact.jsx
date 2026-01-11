a02ade94aff1b42cbd0561d4bbb1abf00bf3dd36
Cuello <maxcuello123@gmail.com>
2026-01-11 02:58:58 -0500

    se quita animacion y se cambia titulo en contacto

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -31,7 +31,7 @@ export const Contact = () => {
         <section id="contact" className="min-h-screen flex items-center justify-center py-20">
             <RevealOnScroll>
                 <div className="px-4 w-150">
-                    <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">Ponte en contacto</h2>
+                    <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">Contactame</h2>
                     <form action="" className="space-y-6" onSubmit={handleSubmit}>
                         <div className="relative">
                             <input
